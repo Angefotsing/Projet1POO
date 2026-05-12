6a6384c4fde24d04a84e0a2b1ba3b43369dbca7e
Angefotsing <https://github.com/Angefotsing/Ange.git>
2026-05-12 05:59:15 +0200

    Ajout du TP8 moteur de recommandations

---
+++ TP8_IPPlanManager_Recommandation/src/ipplanmanager/CalculateurReseau.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ipplanmanager;
+
+/**
+ *
+ * @author ANGELA
+ */
+public class CalculateurReseau {   
+    public static int calculerNombreHotes(int cidr) {   
+        if (cidr < 0 || cidr > 32) {        
+            return 0;        
+        }         int bitsHotes = 32 - cidr;      
+        if (bitsHotes == 0) {        
+            return 1;        
+        }     
+        return (int) Math.pow(2, bitsHotes) - 2;    
+    }    
+    public static int calculerCidrPourHotes(int nombreHotes) {       
+        for (int cidr = 32; cidr >= 0; cidr--) {           
+            int capacite = calculerNombreHotes(cidr);          
+            if (capacite >= nombreHotes) {              
+                return cidr;           
+            }       
+        }        
+        return -1;  
+    }   
+    public static String obtenirMasqueDecimal(int cidr) {    
+        int masque = 0xffffffff << (32 - cidr);        
+        int octet1 = (masque >>> 24) & 255;      
+        int octet2 = (masque >>> 16) & 255;      
+        int octet3 = (masque >>> 8) & 255;       
+        int octet4 = masque & 255;       
+        return octet1 + "." + octet2 + "." + octet3 + "." + octet4;     
+    }  
+    public static int convertirIpEnEntier(String ip) {     
+        String[] parties = ip.split("\\.");      
+        int resultat = 0;      
+        for (int i = 0; i < 4; i++) {  
+            resultat = resultat * 256 + Integer.parseInt(parties[i]);        
+        }     
+        return resultat; 
+    }    
+    public static String convertirEntierEnIp(int valeur) {  
+        int octet1 = (valeur >>> 24) & 255;       
+        int octet2 = (valeur >>> 16) & 255;       
+        int octet3 = (valeur >>> 8) & 255;   
+        int octet4 = valeur & 255; 
+        return octet1 + "." + octet2 + "." + octet3 + "." + octet4;   
+    }  
+    public static int calculerTailleBloc(int cidr) { 
+        return (int) Math.pow(2, 32 - cidr);    
+    } 
+    public static String obtenirClasseReseau(String ip) {
+    String[] parties = ip.split("\\.");
+    int premierOctet = Integer.parseInt(parties[0]);
+
+    if (premierOctet >= 1 && premierOctet <= 126) return "Classe A";
+    if (premierOctet >= 128 && premierOctet <= 191) return "Classe B";
+    if (premierOctet >= 192 && premierOctet <= 223) return "Classe C";
+    
+    return "Classe inconnue ou réservée";
+    }
+    public static String calculerPremiereAdresseUtilisable(String adresseReseau) {
+    int adresseInt = convertirIpEnEntier(adresseReseau);
+    return convertirEntierEnIp(adresseInt + 1);
+    }
+    public static String calculerDerniereAdresseUtilisable(String adresseReseau, int cidr) {
+    int adresseInt = convertirIpEnEntier(adresseReseau);
+    int taille = calculerTailleBloc(cidr);
+    // Adresse de broadcast est adresse + taille - 1, donc dernière utilisable est - 2
+    return convertirEntierEnIp(adresseInt + taille - 2);
+    }
+} 
